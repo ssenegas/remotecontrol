f96133bb6d9973fad27f8d2c0cecbeac79fbfdf4
ssenegas <ssenegas@gmail.com>
2025-03-03 17:19:57 +0100

    No need for a manual mapping of commands anymore since it's embedded in the enum.

--- src/main/java/org/senegas/remotecontrol/view/RemoteController.java
+++ src/main/java/org/senegas/remotecontrol/view/RemoteController.java
@@ -10,11 +10,6 @@ import java.util.Map;
 
 public class RemoteController {
     private static final Logger LOGGER = Logger.getLogger(RemoteController.class.getName());
-    private Map<RemoteButton, Command> commandMap = new HashMap<>();
-
-    public void setCommand(RemoteButton button, Command command) {
-        commandMap.put(button, command);
-    }
 
     public void buttonPressed(RemoteButton button) {
         button.getCommand().execute();
